63a1a56050783ba1c3510b12aa82283f02bd91b8
Lukasz <lukas.kochanovski@wp.pl>
2019-09-12 16:50:45 +0200

    wersja1

---
+++ GraZaDuzoZaMalo/Monolitycznie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1. komputer losuje liczbę z podanego zakresu
+            Random los = new Random();
+            
+
+            // powtarzaj wielokrotnie ąz odgadnie
+            // 2. czlowiek proponuje (odgaduje)
+
+            //3. komputer ocenia propozycję
+            // koniec powtarzaj
+
+            //4. wypisz statystyki gry
+
+        }
+    }
+}
